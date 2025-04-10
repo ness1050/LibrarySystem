0fa874078817a734070218fdd4604fd6c53046fb
Naseem Qurbanli <nq222af@student.lnu.se>
2025-04-10 04:05:26 +0200

    Added Getter and setters

--- src/model/Member.java
+++ src/model/Member.java
@@ -25,8 +25,68 @@ public class Member {
         this.email = email;
     }
 
-    
 
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumebr() {
+        return this.numebr;
+    }
+
+    public void setNumebr(String numebr) {
+        this.numebr = numebr;
+    }
+
+    public int getCredits() {
+        return this.Credits;
+    }
+
+    public void setCredits(int Credits) {
+        this.Credits = Credits;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " firstName='" + firstName + "'" +
+            ", lastName='" + lastName + "'" +
+            ", id='" + id + "'" +
+            ", email='" + email + "'" +
+            ", numebr='" + numebr + "'" +
+            ", Credits='" + Credits + "'" +
+            "}";
+    }
 
 
     
