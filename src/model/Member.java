72cd7734cce3c86be9d92b6650064a6ec137b2fd
Naseem Qurbanli <nq222af@student.lnu.se>
2025-04-10 04:00:05 +0200

    adding to member class

---
+++ src/model/Member.java
@@ -0,0 +1,34 @@
+package model;
+
+/**
+ * Member Class.
+ */
+public class Member {
+
+    
+    private String firstName;
+    private String lastName;
+    private String id;
+    private String email;
+    private String numebr;
+    private int Credits;
+
+    /**
+     * Constructor.
+     * @param fname requirment
+     * @param lname requirment
+     * @param email recommended
+     */
+    public Member(String fname, String lname, String email) {
+        this.firstName = fname;
+        this.lastName = lname;
+        this.email = email;
+    }
+
+    
+
+
+
+    
+    
+}
